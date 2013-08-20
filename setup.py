175c9aa55518ff2ebd83b53a670926d6c323e5ba
Peter Sagerson <psagers@ignorare.net>
2013-08-19 21:53:04 -0700

    Construct the cookie name from a hash of the username.
    Usernames can have characters that are not valid in cookie names these days. Verison 0.1.7.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='django-agent-trust',
-    version='0.1.6',
+    version='0.1.7',
     description=u"A framework for managing agent trust, such as public vs. private computers.",
     long_description=open('README').read(),
     author='Peter Sagerson',
--- test/exotic_user/testproj/app/models.py
+++ test/exotic_user/testproj/app/models.py
@@ -15,7 +15,7 @@ class TestUserManager(BaseUserManager):
 
 class TestUser(AbstractBaseUser):
     identifier = models.CharField(max_length=40, unique=True, db_index=True)
-    mystery_field = models.IntegerField()
+    mystery_field = models.IntegerField(default=0)
 
     objects = TestUserManager()
 
