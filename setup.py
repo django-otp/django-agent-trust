3a1781218c22108ebe7c2b6f94233ff81af71c74
Peter Sagerson <psagers@ignorare.net>
2016-09-04 09:17:18 -0700

    Version 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-agent-trust',
-    version='0.2.1',
+    version='0.2.2',
     description="A framework for managing agent trust, such as public vs. private computers.",
     long_description=open('README').read(),
     author='Peter Sagerson',
